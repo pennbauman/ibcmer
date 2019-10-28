a3e4c9827ffe817edaa9b116fe37f0aad30281ed
Penn Bauman <pennbauman@protonmail.com>
2019-10-28 14:44:34 -0400

    updated install script to support gcc

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
+# Penn Bauman
+# pennbauman@protonmail.com
 
-make final
+if clang++ --version &> /dev/null; then
+	echo "Making (clang++)"
+	clang++ -O2 ibcmer.cpp program.cpp doubleByte.cpp -o ibcmer
+else
+	echo "Making (g++)"
+	g++ -O2 ibcmer.cpp program.cpp doubleByte.cpp -o ibcmer
+fi
 
 sudo cp ibcmer /bin/
